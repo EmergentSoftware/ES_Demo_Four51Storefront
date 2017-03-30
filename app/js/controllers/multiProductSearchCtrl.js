1d3dd408710b06f435ecaafed6d5afa7c5572fe8
mitchs42 <mitch.stewart.io@gmail.com>
2017-03-30 10:26:30 -0500

    Reference Error Fix Attempt
    Uncaught ReferenceError: $scope is not defined

--- app/js/controllers/multiProductSearchCtrl.js
+++ app/js/controllers/multiProductSearchCtrl.js
@@ -1,5 +1,5 @@
-﻿four51.app.controller('MultiProductSearchCtrl', ['$scope', 'Product', 'Category', '$routeParams', '$log',
-function ($scope, Product, Category, $routeParams, $log) {
+﻿four51.app.controller('MultiProductSearchCtrl', ['$scope', 'MultiProduct', 'Product', 'Category', '$routeParams', '$log',
+function ($scope, MultiProduct, Product, Category, $routeParams, $log) {
     $scope.settings = {
         currentPage: 1,
         pageSize: 40
@@ -27,24 +27,24 @@ function ($scope, Product, Category, $routeParams, $log) {
     });
 
     function searchByCategoryInteropID(categoryInteropID) {
-        Product.search(categoryInteropID, $scope.searchTerm, null, function (products, count) {
-
-            if ($scope.minPrice && products) {
-                products = products.filter(product => product.StandardPriceSchedule.PriceBreaks[0].Price >= $scope.minPrice);
+        MultiProduct.search(categoryInteropID, $scope.searchTerm, null, function (products, count, pagesize, minPrice, maxPrice, staticSpecGroup) {
+            $log.log('minPrice: ' + minPrice + ' maxPrice: ' + maxPrice + ' staticSpecGroup: ' + staticSpecGroup);
+            if (minPrice && products) {
+                products = products.filter(product => product.StandardPriceSchedule.PriceBreaks[0].Price >= minPrice);
             }
 
-            if ($scope.maxPrice && products) {
-                products = products.filter(product => product.StandardPriceSchedule.PriceBreaks[0].Price <= $scope.maxPrice);
+            if (maxPrice && products) {
+                products = products.filter(product => product.StandardPriceSchedule.PriceBreaks[0].Price <= maxPrice);
             }
 
-            if ($scope.staticSpecGroup && products) {
-                products = products.filter(product => product.StaticSpecGroups.VisibleSpecGroups[0].Name.toLowerCase() == $scope.staticSpecGroup.toLowerCase());
+            if (staticSpecGroup && products) {
+                products = products.filter(product => product.StaticSpecGroups.VisibleSpecGroups[0].Name.toLowerCase() == staticSpecGroup.toLowerCase());
             }
 
             $scope.products = products;
             $scope.productCount = count;
             $scope.searchLoading = false;
-        }, $scope.settings.currentPage, $scope.settings.pageSize);
+        }, $scope.settings.currentPage, $scope.settings.pageSize, $scope.minPrice, $scope.maxPrice, $scope.staticSpecGroup);
     }
 
     function Search() {
