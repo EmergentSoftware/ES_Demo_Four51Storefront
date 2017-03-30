9e683b9df775afd13f8d5f63a4161e817d817034
mitchs42 <mitch.stewart.io@gmail.com>
2017-03-30 11:08:26 -0500

    Uncaught ReferenceError: Product is not defined
    Compilation Take 2

--- app/js/controllers/multiProductSearchCtrl.js
+++ app/js/controllers/multiProductSearchCtrl.js
@@ -1,5 +1,5 @@
-﻿four51.app.controller('MultiProductSearchCtrl', ['$scope', 'Product', 'Category', '$routeParams', '$log',
-function ($scope, Product, Category, $routeParams, $log) {
+﻿four51.app.controller('MultiProductSearchCtrl', ['$scope', 'Product', '$routeParams',
+function ($scope, Product, $routeParams) {
     $scope.settings = {
         currentPage: 1,
         pageSize: 40
@@ -26,49 +26,12 @@ function ($scope, Product, Category, $routeParams, $log) {
             Search();
     });
 
-    //function searchByCategoryInteropID(categoryInteropID) {
-    //    //MultiProduct.search(categoryInteropID, $scope.searchTerm, null, function (products, count, pagesize, minPrice, maxPrice, staticSpecGroup) {
-    //    Product.search(categoryInteropID, $scope.searchTerm, null, function (products, count, pagesize) {
-    //        //$log.log('minPrice: ' + minPrice + ' maxPrice: ' + maxPrice + ' staticSpecGroup: ' + staticSpecGroup);
-    //        //if (minPrice && products) {
-    //        //    products = products.filter(product => product.StandardPriceSchedule.PriceBreaks[0].Price >= minPrice);
-    //        //}
-
-    //        //if (maxPrice && products) {
-    //        //    products = products.filter(product => product.StandardPriceSchedule.PriceBreaks[0].Price <= maxPrice);
-    //        //}
-
-    //        //if (staticSpecGroup && products) {
-    //        //    products = products.filter(product => product.StaticSpecGroups.VisibleSpecGroups[0].Name.toLowerCase() == staticSpecGroup.toLowerCase());
-    //        //}
-
-    //        $scope.products = products;
-    //        $scope.productCount = count;
-    //        $scope.searchLoading = false;
-    //        //}, $scope.settings.currentPage, $scope.settings.pageSize, $scope.minPrice, $scope.maxPrice, $scope.staticSpecGroup);
-    //    //}, $scope.settings.currentPage, $scope.settings.pageSize, 0, 50.00, null);
-    //    }, $scope.settings.currentPage, $scope.settings.pageSize);
-    //}
-
     function Search() {
         $scope.searchLoading = true;
-        if ($scope.searchCategories) {
-            Object.keys(categories).forEach((element, index, array) => {
-                if ($scope.searchCategories.split(',').includes(element)) {
-                    //searchByCategoryInteropID(categories[element].categoryInteropID);
-                    Product.search(categoryInteropID, $scope.searchTerm, null, function (products, count, pagesize) {
-                        $scope.products = products;
-                        $scope.productCount = count;
-                        $scope.searchLoading = false;
-                    }, $scope.settings.currentPage, $scope.settings.pageSize);
-                }
-            })
-        } else {
-            Product.search(null, $scope.searchTerm, null, function (products, count) {
-                $scope.products = products;
-                $scope.productCount = count;
-                $scope.searchLoading = false;
-            }, $scope.settings.currentPage, $scope.settings.pageSize);
-        }
+        Product.search(null, $scope.searchTerm, null, function (products, count) {
+            $scope.products = products;
+            $scope.productCount = count;
+            $scope.searchLoading = false;
+        }, $scope.settings.currentPage, $scope.settings.pageSize);
     }
 }]);
